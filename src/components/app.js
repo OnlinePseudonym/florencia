0c36ec01c4ff154a303d94cbcddcf1d52230346c
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-04-08 20:09:53 -0700

    style landing

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Nav from "./nav";
 import Landing from "./landing";
 import Location from "./location";
 import About from "./about";
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <Landing />
         <Location />
         <About />
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import "./landing.css";
 
 const Landing = () => {
     return (
-        <div>You have landed</div>
+        <div className="landing">
+            <h1 className="landing--title-primary">Florencia</h1>
+            <h2 className="landing--title-secondary">the Pizza Bistro</h2>
+        </div>
     )
 }
 
