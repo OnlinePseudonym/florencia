0757c86c4328f1e9fb8eb35a6dc8cedfce4d90bd
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-04-08 20:25:06 -0700

    add basic nav

--- src/components/landing.js
+++ src/components/landing.js
@@ -3,7 +3,7 @@ import "./landing.css";
 
 const Landing = () => {
     return (
-        <div className="landing">
+        <div id="landing" className="landing">
             <h1 className="landing--title-primary">Florencia</h1>
             <h2 className="landing--title-secondary">the Pizza Bistro</h2>
         </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import "./nav.css";
 
 const Nav = () => {
     return (
         <nav className="nav">
+            <h2 className="nav--logo"><a href="#landing">FPB</a></h2>
+            <a href="#location" className="nav__item">LOCATION</a>
+            <a href="#about" className="nav__item">ABOUT</a>
+            <a href="#menu" className="nav__item">MENU</a>
         </nav>
     )
 }
