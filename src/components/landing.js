0188069871578afbea36dc5de51644ef316619a7
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-04-16 19:27:43 -0700

    new landing background

--- src/components/landing.js
+++ src/components/landing.js
@@ -7,6 +7,7 @@ const Landing = () => {
         <div id="landing" className="landing">
             <h1 className="landing__title">Florencia</h1>
             <h2 className="landing__title--secondary">the Pizza Bistro</h2>
+            <a className="landing__button" href="#menu">View Menu</a>
             <Divider className="divider--steep divider--blue divider--bottom" />
         </div>
     )
